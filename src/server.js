ca90743b7ed600aa0bbebead2e04d39f687d9026
Matej <mlipcinski@abv.bg>
2018-11-15 13:24:47 +0200

    add dist directory for js files

--- src/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-var PORT = 7000;
-app_1.default.listen(PORT, function () {
-    console.log('Express server listening on port ' + PORT);
-});
-//# sourceMappingURL=server.js.map
